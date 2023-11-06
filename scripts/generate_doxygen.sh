ddf1554cf9b4284d6faa64ef9e84c5195537136e
David Chisnall <github@theravensnest.org>
2023-11-06 12:53:38 +0000

    Hopefully actually fix the container name this time.

--- scripts/generate_doxygen.sh
+++ scripts/generate_doxygen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-podman run --mount type=bind,src=$(realpath $(dirname $0)/..),target=/mnt --rm ghcr.io/cheriot-platform/book-build-container bash -c 'cd /mnt/ && /doxygen/bin/doxygen'
+podman run --mount type=bind,src=$(realpath $(dirname $0)/..),target=/mnt --rm ghcr.io/cheriot-platform/book-build-container:main bash -c 'cd /mnt/ && /doxygen/bin/doxygen'
