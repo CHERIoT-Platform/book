6f6c0b143736433dff39749986b312d40f05a08a
David Chisnall <github@theravensnest.org>
2023-11-06 15:24:41 +0000

    Don't try to put the build files on the web.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-podman run --mount type=bind,src=$(realpath $(dirname $0)/..),target=/mnt --rm ghcr.io/cheriot-platform/book-build-container:main sh -c 'cd /mnt && asciidoxy --spec-file packages.toml --base-dir text text/index.adoc -D build  -b pdf -r asciidoctor-pdf --template-dir templates && mv build/index.pdf build/cheriot-programmers-guide.pdf'
-podman run --mount type=bind,src=$(realpath $(dirname $0)/..),target=/mnt --rm ghcr.io/cheriot-platform/book-build-container:main sh -c 'cd /mnt && asciidoxy --spec-file packages.toml --base-dir text text/index.adoc -D build --template-dir templates --multipage'
+podman run --mount type=bind,src=$(realpath $(dirname $0)/..),target=/mnt --rm ghcr.io/cheriot-platform/book-build-container:main sh -c 'cd /mnt && asciidoxy --spec-file packages.toml --base-dir text text/index.adoc -D _site --build-dir build  -b pdf -r asciidoctor-pdf --template-dir templates && mv build/index.pdf build/cheriot-programmers-guide.pdf'
+podman run --mount type=bind,src=$(realpath $(dirname $0)/..),target=/mnt --rm ghcr.io/cheriot-platform/book-build-container:main sh -c 'cd /mnt && asciidoxy --spec-file packages.toml --base-dir text text/index.adoc -D _site --build-dir build --template-dir templates --multipage'
 
