40e5d56329ac2443b43e66cccf635e2d045f02d7
David Chisnall <github@theravensnest.org>
2024-11-28 17:43:34 +0000

    Simplify some code.

--- lua/html-em.lua
+++ lua/html-em.lua
@@ -1,17 +1,7 @@
-
-function visit(textTree)
-	if type(textTree) ~= "string" then
-		if (textTree.kind == "textem") then
-			textTree.kind = "em"
-		end
-		textTree:visit(visit)
-	end
-	return {textTree}
-end
-
-
 function process(textTree)
-	textTree:visit(visit)
+	textTree:match("textem", function(tree)
+		tree.kind = "em"
+		return { tree }
+	end)
 	return textTree
 end
-
