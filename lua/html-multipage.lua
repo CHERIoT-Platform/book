72a1ad7f69f2be7af50b42f8d69cf363bc5912d5
David Chisnall <github@theravensnest.org>
2024-12-04 12:29:45 +0000

    Finish basic multipage output.

---
+++ lua/html-multipage.lua
@@ -0,0 +1,131 @@
+local chapters = {}
+local labelsByFile = {}
+
+function newChapter(name, file)
+	return {
+		name = name,
+		file = file,
+		root = TextTree.new(),
+		sections = TextTree.new("ul"),
+	}
+end
+
+local currentChapter = nil
+local toc = TextTree.new()
+
+function pushChapter(name, file)
+	if currentChapter then
+		table.insert(chapters, currentChapter)
+	end
+	currentChapter = newChapter(name, file)
+end
+
+function pushNode(node)
+	if currentChapter == nil then
+		currentChapter = newChapter("Frontmatter", "frontmatter.html")
+	end
+	currentChapter.root:append_child(node)
+end
+
+function collectLabels(textTree)
+	if type(textTree) ~= "string" then
+		if textTree:has_attribute("label") then
+			local label = textTree:attribute("label")
+			labelsByFile["#" .. label] = currentChapter.file
+		end
+		textTree:visit(collectLabels)
+	end
+	return { textTree }
+end
+
+function collectChapters(textTree)
+	if type(textTree) ~= "string" then
+		if textTree.kind == "chapter" then
+			local chapterFile = textTree:source_file_name()
+			local _, _, suffix = string.find(chapterFile, ".*%.(.*)")
+			-- Remove the source suffix and add .html
+			chapterFile = string.sub(chapterFile, 1, #chapterFile - #suffix) .. "html"
+			-- Remove path
+			chapterFile = string.gsub(chapterFile, ".*/", "")
+			pushChapter(textTree:text(), chapterFile)
+		elseif textTree.kind == "section" then
+			local li = currentChapter.sections:new_child("li")
+			local a = li:new_child("a")
+			a:attribute_set("href", "#" .. textTree:attribute("label"))
+			a:take_children(textTree:deep_clone())
+		end
+		collectLabels(textTree)
+	end
+	pushNode(textTree)
+	return {}
+end
+
+function process(textTree)
+	-- Split out the chapters
+	textTree:visit(collectChapters)
+	table.insert(chapters, currentChapter)
+	currentChapter = nil
+	-- Rewrite all links to be to the right file, if they point to a different
+	-- file.
+	for _, chapter in pairs(chapters) do
+		chapter.root:match("a", function(node)
+			if node:has_attribute("href") then
+				local label = node:attribute("href")
+				local labelFile = labelsByFile[label]
+				if labelFile ~= chapter.file then
+					label = labelFile .. label
+					node:attribute_set("href", label)
+				end
+			end
+			return { node }
+		end)
+	end
+	-- Run the passes to generate the final output
+	for _, chapter in pairs(chapters) do
+		local tinyTOC = TextTree.new("ul")
+		for _, chapterForToC in pairs(chapters) do
+			local li = tinyTOC:new_child("li")
+			local a = li:new_child("a")
+			a:attribute_set("href", chapterForToC.file)
+			a:append_text(chapterForToC.name)
+			if chapter == chapterForToC then
+				li:append_child(chapter.sections)
+			end
+		end
+		local root = TextTree.create({
+			kind = "div",
+			attributes = {
+				class = "body",
+			},
+			children = {
+				{
+					kind = "div",
+					attributes = {
+						class = "minitoc",
+					},
+					children = { tinyTOC },
+				},
+				{
+					kind = "div",
+					attributes = {
+						class = "content",
+					},
+					children = { chapter.root },
+				},
+			},
+		})
+		root = create_pass("html-headings"):process(root)
+		root = create_pass("html-label-id"):process(root)
+		root = create_pass("html-boilerplate"):process(root)
+		root = create_pass("clean-empty"):process(root)
+		root = create_pass("html-hoist-pre"):process(root)
+		root = create_pass("html-cleanup"):process(root)
+		local out = create_pass("HTMLOutputPass"):as_output_pass()
+		if root then
+			print("Writing " .. chapter.file)
+			out:output_file(chapter.file)
+			out:process(root)
+		end
+	end
+	return TextTree.new()
+end
