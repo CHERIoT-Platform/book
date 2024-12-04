8f5d67319ac90415b049125620479f8de1187f1a
David Chisnall <github@theravensnest.org>
2024-12-04 12:29:27 +0000

    Add the correct attribute for code formatting.

--- lua/html-lua.lua
+++ lua/html-lua.lua
@@ -1,11 +1,17 @@
 function visit(tree)
 	if type(tree) ~= "string" then
 		if tree.kind == "cxxsnippet" then
+			tree:attribute_set("class", "listing-code")
+			tree.kind = "pre"
+		elseif tree.kind == "jsonsnippet" then
+			tree:attribute_set("class", "listing-code")
 			tree.kind = "pre"
 		elseif tree.kind == "luasnippet" then
+			tree:attribute_set("class", "listing-code")
 			tree.kind = "pre"
 		elseif tree.kind == "lualisting" then
 			tree.kind = "pre"
+			tree:attribute_set("class", "listing-code")
 			tree:append_text("FIXME: Lua listings not implemented yet")
 		else
 			tree:visit(visit)
