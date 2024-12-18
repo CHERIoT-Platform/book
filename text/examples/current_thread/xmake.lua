d11bb75e17e01d51601eb23c7e2bec72c903a588
David Chisnall <github@theravensnest.org>
2024-12-18 14:21:21 +0000

    Add missing examples from last commit.  Ooops.

---
+++ text/examples/current_thread/xmake.lua
@@ -0,0 +1,50 @@
+-- Copyright Microsoft and CHERIoT Contributors.
+-- SPDX-License-Identifier: MIT
+
+set_project("CHERIoT example")
+
+sdkdir = os.getenv("CHERIOT_SDK") or
+	"../../../rtos-source/sdk/"
+includes(sdkdir)
+
+set_toolchains("cheriot-clang")
+
+option("board")
+    set_default("sail")
+
+-- An single compartment for this example.
+compartment("current")
+    add_files("current.cc")
+
+
+-- firmware#begin
+-- Firmware image for the example.
+firmware("current_thread")
+    -- RTOS-provided libraries
+    add_deps("freestanding", "stdio")
+    -- Our compartments
+    add_deps("current")
+    on_load(function(target)
+        -- The board to target
+        target:values_set("board", "$(board)")
+        -- threads#begin
+        -- Threads to select
+        target:values_set("threads", {
+            {
+                compartment = "current",
+                priority = 1,
+                entry_point = "entry",
+                stack_size = 0x400,
+                trusted_stack_frames = 2
+            },
+            {
+                compartment = "current",
+                priority = 2,
+                entry_point = "entry",
+                stack_size = 0x400,
+                trusted_stack_frames = 2
+            }
+        }, {expand = false})
+        -- threads#end
+    end)
+-- firmware#end
