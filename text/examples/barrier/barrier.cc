e1e81f169669c674541f724ec05f68327e8d9c78
David Chisnall <github@theravensnest.org>
2024-12-20 13:37:24 +0000

    Add more examples of locking.

---
+++ text/examples/barrier/barrier.cc
@@ -0,0 +1,36 @@
+// Copyright Microsoft and CHERIoT
+// Contributors.
+// SPDX-License-Identifier: MIT
+
+#include <atomic>
+#include <stdio.h>
+#include <thread.h>
+
+// entry#begin
+/// Thread entry point.
+__cheri_compartment(
+  "barrier") void entry()
+{
+	static std::atomic<uint32_t>
+	  barrier = 2;
+	printf(
+	  "Thread: %d arrived at barrier\n",
+	  thread_id_get());
+	uint32_t value = --barrier;
+	if (value == 0)
+	{
+		barrier.notify_all();
+	}
+	else
+	{
+		while (value != 0)
+		{
+			barrier.wait(value);
+			value = barrier;
+		}
+	}
+	printf(
+	  "Thread: %d passed barrier\n",
+	  thread_id_get());
+}
+// entry#end
---
+++ text/examples/locking/xmake.lua
@@ -0,0 +1,57 @@
+-- Copyright Microsoft and CHERIoT Contributors.
+-- SPDX-License-Identifier: MIT
+
+set_project("CHERIoT example")
+
+sdkdir = os.getenv("CHERIOT_SDK") or
+	"../../../rtos-source/sdk/"
+includes(sdkdir)
+
+set_toolchains("cheriot-clang")
+
+option("board")
+    set_default("sail")
+
+-- An single compartment for this example.
+compartment("locking")
+    add_files("locking.cc")
+
+
+-- firmware#begin
+-- Firmware image for the example.
+firmware("locking_thread")
+    -- RTOS-provided libraries
+    add_deps("freestanding", "stdio")
+    -- Our compartments
+    add_deps("locking")
+    on_load(function(target)
+        -- The board to target
+        target:values_set("board", "$(board)")
+        -- threads#begin
+        -- Threads to select
+        target:values_set("threads", {
+            {
+                compartment = "locking",
+                priority = 1,
+                entry_point = "low",
+                stack_size = 0x400,
+                trusted_stack_frames = 2
+            },
+            {
+                compartment = "locking",
+                priority = 2,
+                entry_point = "medium",
+                stack_size = 0x400,
+                trusted_stack_frames = 2
+            },
+            {
+                compartment = "locking",
+                priority = 3,
+                entry_point = "high",
+                stack_size = 0x400,
+                trusted_stack_frames = 2
+            }
+        }, {expand = false})
+        -- threads#end
+    end)
+-- firmware#end
