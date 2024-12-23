f554a37faffc238b5beadd957f136e510f616613
David Chisnall <github@theravensnest.org>
2024-12-23 12:34:34 +0000

    Add examples for compartment export language extensions.

---
+++ text/examples/c_extensions/entry.cc
@@ -0,0 +1,23 @@
+// Copyright Microsoft and CHERIoT Contributors.
+// SPDX-License-Identifier: MIT
+
+#include "interface.h"
+#include <stdio.h>
+
+// callback#begin
+int __cheri_callback callback(int counter)
+{
+	printf("Counter value: %d\n", counter);
+	return 0;
+}
+// callback#end
+
+/// Thread entry point.
+void __cheri_compartment("entry") entry()
+{
+// compartment_call#begin
+	increment();
+	monotonic(callback);
+	monotonic(&callback);
+// compartment_call#end
+}
