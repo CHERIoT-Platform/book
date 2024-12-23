b2156b8e3cb3a81668ecc7b12d00d4426ae12822
David Chisnall <github@theravensnest.org>
2024-12-23 12:35:15 +0000

    Use tabs in xmake.lua.
    We set tabs to two characters in the CSS / PDF, so this wastes less
    horizontal space when it's constrained.

--- text/examples/locking/xmake.lua
+++ text/examples/locking/xmake.lua
@@ -10,48 +10,48 @@ includes(sdkdir)
 set_toolchains("cheriot-clang")
 
 option("board")
-    set_default("sail")
+	set_default("sail")
 
 -- An single compartment for this example.
 compartment("locking")
-    add_files("locking.cc")
+	add_files("locking.cc")
 
 
 -- firmware#begin
 -- Firmware image for the example.
 firmware("locking_thread")
-    -- RTOS-provided libraries
-    add_deps("freestanding", "stdio")
-    -- Our compartments
-    add_deps("locking")
-    on_load(function(target)
-        -- The board to target
-        target:values_set("board", "$(board)")
-        -- threads#begin
-        -- Threads to select
-        target:values_set("threads", {
-            {
-                compartment = "locking",
-                priority = 1,
-                entry_point = "low",
-                stack_size = 0x400,
-                trusted_stack_frames = 2
-            },
-            {
-                compartment = "locking",
-                priority = 2,
-                entry_point = "medium",
-                stack_size = 0x400,
-                trusted_stack_frames = 2
-            },
-            {
-                compartment = "locking",
-                priority = 3,
-                entry_point = "high",
-                stack_size = 0x400,
-                trusted_stack_frames = 2
-            }
-        }, {expand = false})
-        -- threads#end
-    end)
+	-- RTOS-provided libraries
+	add_deps("freestanding", "stdio")
+	-- Our compartments
+	add_deps("locking")
+	on_load(function(target)
+		-- The board to target
+		target:values_set("board", "$(board)")
+		-- threads#begin
+		-- Threads to select
+		target:values_set("threads", {
+			{
+				compartment = "locking",
+				priority = 1,
+				entry_point = "low",
+				stack_size = 0x400,
+				trusted_stack_frames = 2
+			},
+			{
+				compartment = "locking",
+				priority = 2,
+				entry_point = "medium",
+				stack_size = 0x400,
+				trusted_stack_frames = 2
+			},
+			{
+				compartment = "locking",
+				priority = 3,
+				entry_point = "high",
+				stack_size = 0x400,
+				trusted_stack_frames = 2
+			}
+		}, {expand = false})
+		-- threads#end
+	end)
 -- firmware#end
