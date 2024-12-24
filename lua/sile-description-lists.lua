696949d2ed7612d3870941cf48494dffe6f09be4
David Chisnall <github@theravensnest.org>
2024-12-24 12:55:45 +0000

    More language extension docs.

--- lua/sile-description-lists.lua
+++ lua/sile-description-lists.lua
@@ -1,21 +1,17 @@
 local visit
 
 function itemTitle(textTree)
-	if not (type(textTree) == "string") then
-		if (textTree.kind == "item") then
-			local dt = TextTree.new("font")
-			if not textTree:has_attribute("tag") then
-				textTree:error("missing tag attribute")
-				return {textTree}
-			end
-			dt:attribute_set("weight", "700")
-			dt:append_text(textTree:attribute("tag"))
-			textTree:attribute_erase("tag")
-			table.insert(textTree.children, 1, dt)
-			textTree:visit(visit)
-			return {textTree}
-		end
+	local dt = TextTree.new("font")
+	if not textTree:has_attribute("tag") then
+		textTree:error("missing tag attribute")
+		return {textTree}
 	end
+	dt:attribute_set("weight", "700")
+	dt:append_text(textTree:attribute("tag"))
+	dt:append_text(" ")
+	textTree:attribute_erase("tag")
+	table.insert(textTree.children, 1, dt)
+	textTree:visit(visit)
 	return {textTree}
 end
 
@@ -24,7 +20,8 @@ visit = function (textTree)
 		if (textTree.kind == "description") then
 			textTree.kind = "itemize"
 			textTree:attribute_set("bullet", "")
-			textTree:visit(itemTitle)
+			textTree:match("item", itemTitle)
+			return { textTree }
 		else
 			textTree:visit(visit)
 		end
---
+++ text/examples/library_annotation/interface.h
@@ -0,0 +1,11 @@
+// Copyright Contributors.
+// SPDX-License-Identifier: MIT
+
+#include <compartment.h>
+
+// library_export#begin
+/**
+ * A simple example library function.
+ */
+__cheri_libcall void library_function();
+// library_export#end
