ed76021c50c3c75321dfaba0dcd1fbd6140a87d4
David Chisnall <github@theravensnest.org>
2024-12-28 13:38:01 +0000

    More compartment chapter.

--- text/examples/library_or_compartment/xmake.lua
+++ text/examples/library_or_compartment/xmake.lua
@@ -12,24 +12,31 @@ set_toolchains("cheriot-clang")
 option("board")
 	set_default("sail")
 
+-- compartments_and_libraries#begin
 -- An example compartment that we can call
 compartment("compartment")
 	add_files("compartment.cc")
+target_end()
 
 -- An example compartment that we can call
 library("library")
+	set_default(false)
 	add_files("library.cc")
 
 -- Our entry-point compartment
 compartment("entry")
 	add_files("entry.cc")
+	add_deps("compartment", "library")
+-- compartments_and_libraries#end
 
+-- compartments_as_dependencies#begin
 -- Firmware image for the example.
 firmware("library_or_compartment")
 	-- RTOS-provided libraries
 	add_deps("freestanding", "debug")
 	-- Our compartments
-	add_deps("entry", "compartment", "library")
+	add_deps("entry")
+	-- compartments_as_dependencies#end
 	on_load(function(target)
 		-- The board to target
 		target:values_set("board", "$(board)")
