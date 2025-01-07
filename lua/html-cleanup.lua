89fbfdad38eae6d3a838d4e080801087cdb1628c
David Chisnall <github@theravensnest.org>
2025-01-07 12:10:53 +0000

    Add more images.

--- lua/html-cleanup.lua
+++ lua/html-cleanup.lua
@@ -1,3 +1,13 @@
+function file_exists(name)
+	local f = io.open(name, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
+end
+
 function process(textTree)
 	textTree:match_any({ "h1", "h2", "h3", "h4" }, function(heading)
 		if heading:has_attribute("number") then
@@ -20,8 +30,26 @@ function process(textTree)
 		local figureBlock = TextTree.new("div")
 		figureBlock:attribute_set("class", "figure")
 		local img = figureBlock:new_child("img")
-		img:attribute_set("src", figure:attribute("src"))
-		img:attribute_set("alt", figure:attribute("alt"))
+		local src = figure:attribute("src")
+		-- SVGs may include other files.  Some of the SVGs are actually
+		-- directories, look for the real file inside.
+		local nestedSVG = string.gsub(src, "(.*/)(.*%.svg)$", "%1%2/%2")
+		if file_exists(nestedSVG) then
+			src = nestedSVG
+		end
+
+		-- SVG images need special handling because modern web specs are a
+		-- mess.  At least here the major browsers are consistently weird.
+		if string.match(src, "%.svg$") then
+			img.kind = "object"
+			img:attribute_set("data", src)
+			img:attribute_set("type", "image/svg+xml")
+			img:attribute_set("aria-label", figure:attribute("alt"))
+		else
+			img:attribute_set("src", src)
+			img:attribute_set("alt", figure:attribute("alt"))
+		end
+
 		if figure:has_attribute("number") then
 			figure:insert_text(1, "Figure " .. figure:attribute("number") .. ". ")
 		end
