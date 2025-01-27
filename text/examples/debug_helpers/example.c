8dd9339fcc979d491bb8098fd98a18816b8a452a
David Chisnall <github@theravensnest.org>
2025-01-27 17:47:22 +0000

    clang-format.

--- text/examples/debug_helpers/example.c
+++ text/examples/debug_helpers/example.c
@@ -1,15 +1,18 @@
 #include <debug.h>
-#include <unwind.h>
 #include <stdio.h>
+#include <unwind.h>
 
 void print_from_c(void)
 {
 	// all#begin
-	CHERIOT_DEBUG_LOG("C example", "Printing a number {} and a string {}",
-			42, "hello from C");
+	CHERIOT_DEBUG_LOG("C example",
+	                  "Printing a number {} and a string {}",
+	                  42,
+	                  "hello from C");
 	CHERIOT_DURING
 	{
-		CHERIOT_INVARIANT(false, "Invariant check in C failed: {}", 12);
+		CHERIOT_INVARIANT(
+		  false, "Invariant check in C failed: {}", 12);
 	}
 	CHERIOT_HANDLER
 	{
