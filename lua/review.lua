e2fe89f0c245004622f29f7cb41c3237ea4f77f8
David Chisnall <github@theravensnest.org>
2025-02-17 10:51:37 +0000

    Add code for handling review comments.

---
+++ lua/review.lua
@@ -0,0 +1,18 @@
+function process(textTree)
+	textTree:match_any({ "phil" }, function(review)
+		if not config.review then
+			return {}
+		end
+		table.insert(review.children, 1, "[ ")
+		review:append_text(" - " .. review.kind .. " ]")
+		if config.output == "sile" then
+			review.kind = "color"
+			review:attribute_set("color", "blue")
+		else
+			review.kind = "span"
+			review:attribute_set("class", "review")
+		end
+		return { review }
+	end)
+	return textTree
+end
