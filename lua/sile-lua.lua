ba43e51fa81ba8fee465cb85a352126a0d498dfb
David Chisnall <github@theravensnest.org>
2025-02-23 15:03:34 +0000

    Small cleanup to sile typesetting.

--- lua/sile-lua.lua
+++ lua/sile-lua.lua
@@ -1,26 +1,18 @@
+local snippets = {
+	"cxxsnippet",
+	"luasnippet",
+	"console",
+	"asmsnippet",
+	"regosnippet",
+	"jsonsnippet",
+}
 
 function visit(tree)
-	if type(tree) ~= 'string' then
-		if tree.kind == 'cxxsnippet' then
-			tree.kind = 'verbatim'
-		elseif tree.kind == 'luasnippet' then
-			tree.kind = 'verbatim'
-		elseif tree.kind == 'console' then
-			tree.kind = 'verbatim'
-		elseif tree.kind == 'asmsnippet' then
-			tree.kind = 'verbatim'
-		elseif tree.kind == 'regosnippet' then
-			tree.kind = 'verbatim'
-		elseif tree.kind == 'jsonsnippet' then
-			tree.kind = 'verbatim'
-		else
-			tree:visit(visit)
-		end
-	end
+	tree.kind = "verbatim"
 	return {tree}
 end
 
 function process(textTree)
-	textTree:visit(visit)
+	textTree:match_any(snippets, visit)
 	return textTree
 end
