d91419588d8100a92ce26de74dc40a8a9560856e
David Chisnall <github@theravensnest.org>
2025-03-17 12:58:44 +0000

    Move examples to a separate git repository.

--- text/examples/producer_consumer_compartment/consumer.h
+++
@@ -1,9 +0,0 @@
-// Copyright Microsoft and CHERIoT Contributors.
-// SPDX-License-Identifier: MIT
-
-#include <compartment.h>
-#include <cstdlib>
-#include <queue.h>
-
-void __cheriot_compartment("consumer")
-  set_queue(CHERI_SEALED(MessageQueue *) queueHandle);
--- text/examples/safebox/safebox.h
+++
@@ -1,9 +0,0 @@
-#include <compartment-macros.h>
-
-/**
- * Check whether a guess is correct.  The compartment holds
- * a secret value that is a number from 0-9.  Returns true
- * if the guess is correct.
- */
-__cheriot_compartment("safebox") bool check_guess(
-  int guess);
