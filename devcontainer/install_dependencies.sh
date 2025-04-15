8fc193363dca7161fe8d5971a68db63ce40b8a31
David Chisnall <github@theravensnest.org>
2025-04-15 17:37:57 +0100

    Try to use Nix for SILE in CI.

--- devcontainer/install_dependencies.sh
+++ devcontainer/install_dependencies.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 apt update
 echo | apt-get install -y software-properties-common
 add-apt-repository -y ppa:sile-typesetter/sile
 apt update
-echo | apt install -y git wget luarocks make 
-cd /tmp
-wget https://launchpad.net/~sile-typesetter/+archive/ubuntu/sile/+build/30575828/+files/sile_0.15.10-1ppa1~ubuntu24.04_amd64.deb
-dpkg -i ./sile_0.15.10-1ppa1~ubuntu24.04_amd64.deb
-apt -fy install
-dpkg -i ./sile_0.15.10-1ppa1~ubuntu24.04_amd64.deb
+echo | apt install -y git luarocks make nix
+cat << EOF > /bin/sile
+#!/bin/sh
+nix run github:sile-typesetter/sile/v0.15.10 --extra-experimental-features nix-command --extra-experimental-features flakes --
+EOF
+chmod +x /bin/sile
