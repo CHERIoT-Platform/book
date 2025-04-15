f307ffe52f3191f0da1a8fa8187c2527e1e5cf39
David Chisnall <github@theravensnest.org>
2025-04-15 16:39:36 +0100

    Work around SILE breakages.

--- devcontainer/install_dependencies.sh
+++ devcontainer/install_dependencies.sh
@@ -2,5 +2,9 @@ apt update
 echo | apt-get install -y software-properties-common
 add-apt-repository -y ppa:sile-typesetter/sile
 apt update
-echo | apt install -y git git-lfs sile luarocks make epubcheck
-
+echo | apt install -y git wget luarocks make 
+cd /tmp
+wget https://launchpad.net/~sile-typesetter/+archive/ubuntu/sile/+build/30575828/+files/sile_0.15.10-1ppa1~ubuntu24.04_amd64.deb
+dpkg -i ./sile_0.15.10-1ppa1~ubuntu24.04_amd64.deb
+apt -fy install
+dpkg -i ./sile_0.15.10-1ppa1~ubuntu24.04_amd64.deb
